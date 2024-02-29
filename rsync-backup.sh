65e1fcf2ab6677b874937411150a571dc4039ecc
haenno <haenno@web.de>
2024-02-29 16:23:34 +0100

    Update rsync-backup.sh

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -4,5 +4,5 @@
 # 0 3 * * * root bash /home/haenno/haenno.de/rsync-backup.sh     # the script file is running every day at 03:00
 
 /usr/bin/rsync -avz --delete /etc /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-etc.log
-#/usr/bin/rsync -avz --delete --exclude /var/log/journal /var/log /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-log.log 
+/usr/bin/rsync -avz --delete /var/log /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-log.log 
 /usr/bin/rsync -avz --delete /home/haenno /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-haenno.log 
