8f026de853f0d93f789379520f5905f0a2932183
haenno <haenno@web.de>
2024-02-29 23:22:12 +0100

    Update rsync-backup.sh

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -3,6 +3,6 @@
 # add to roots crontab: sudo crontab -e
 # 0 3 * * * root bash /home/haenno/haenno.de/rsync-backup.sh     # the script file is running every day at 03:00
 
-/usr/bin/rsync -avz --delete /etc /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-etc.log
-/usr/bin/rsync -avz --delete /var/log /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-log.log 
-/usr/bin/rsync -avz --delete /home/haenno /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-haenno.log 
+/usr/bin/rsync -a --stats --delete /etc /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-etc.log
+/usr/bin/rsync -a --stats --delete /var/log /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-log.log 
+/usr/bin/rsync -a --stats --delete /home/haenno /mnt/exthdd/sharedir/Raspi-Backup --log-file=/var/log/rsync-daily-backup-haenno.log 
